fe87422b5be2182add156782a25b4b30e0a86c03
yihu <yihu@192.168.1.122>
2014-06-03 10:04:22 -0400

    fix a bug in add query/insertion event periodically

--- event.cpp
+++ event.cpp
@@ -37,13 +37,14 @@ DummyEvent::DummyEvent() : Event()
 
 bool DummyEvent::Callback()	// if return is true, this event can be deleted
 {
-	_time_done += 1;
-	EventScheduler::Inst()->AddEvent(this);
+	
         UINT32 totalNodes = Underlay::Inst()->global_node_table.size();
-        if(_time_created < Settings::QueryHours)
+        if(_time_done < Settings::QueryHours)
             Underlay::Inst()->generateWorkload(1,Settings::QueryPerNode*totalNodes,'Q');
-        if(_time_created<Settings::UpdateHours)
+        if(_time_done<Settings::UpdateHours)
         Underlay::Inst()->generateWorkload(1,Settings::UpdatePerNode*totalNodes,'U');
+        _time_done += 1;
+	EventScheduler::Inst()->AddEvent(this);
 	return false;
 }
 
